28d2d3288f19444db36dd3f2b9107c88558e44f8
sdoshi983 <sdoshi983@rku.ac.in>
2021-11-30 21:27:56 +0530

    Single child scroll view

--- lib/views/chat.dart
+++ lib/views/chat.dart
@@ -14,25 +14,29 @@ class Chat extends StatefulWidget {
 }
 
 class _ChatState extends State<Chat> {
-
   Stream<QuerySnapshot> chats;
   TextEditingController messageEditingController = new TextEditingController();
 
-  Widget chatMessages(){
+  Widget chatMessages() {
     return StreamBuilder(
       stream: chats,
-      builder: (context, snapshot){
-        return snapshot.hasData ?   ListView.builder(
+      builder: (context, snapshot) {
+        return snapshot.hasData
+            ? ListView.builder(
                 reverse: true,
                 itemCount: snapshot.data.docs.length,
-                itemBuilder: (context, index){
+                itemBuilder: (context, index) {
                   return MessageTile(
-                    message: snapshot.data.docs[snapshot.data.docs.length-index-1].data()["message"].toString(),
-                    sendByMe: Constants.myName == snapshot.data.docs[index].data()["sendBy"],
+                    message: snapshot
+                        .data.docs[snapshot.data.docs.length - index - 1]
+                        .data()["message"]
+                        .toString(),
+                    sendByMe: Constants.myName ==
+                        snapshot.data.docs[index].data()["sendBy"],
                   );
                 },
-
-        ) : Container();
+              )
+            : Container();
       },
     );
   }
@@ -42,9 +46,7 @@ class _ChatState extends State<Chat> {
       Map<String, dynamic> chatMessageMap = {
         "sendBy": Constants.myName,
         "message": messageEditingController.text,
-        'time': DateTime
-            .now()
-            .millisecondsSinceEpoch,
+        'time': DateTime.now().millisecondsSinceEpoch,
       };
 
       DatabaseMethods().addMessage(widget.chatRoomId, chatMessageMap);
@@ -69,19 +71,16 @@ class _ChatState extends State<Chat> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: appBarMain(context),
-
       body: Container(
         child: Stack(
           children: [
-            Container(child: chatMessages(),
+            Container(
+              child: chatMessages(),
               padding: EdgeInsets.only(bottom: 65),
             ),
             Container(
               alignment: Alignment.bottomCenter,
-              width: MediaQuery
-                  .of(context)
-                  .size
-                  .width,
+              width: MediaQuery.of(context).size.width,
               child: Container(
                 padding: EdgeInsets.symmetric(horizontal: 24, vertical: 7),
                 color: Color(0xff007EF4),
@@ -89,18 +88,19 @@ class _ChatState extends State<Chat> {
                   children: [
                     Expanded(
                         child: TextField(
-                          controller: messageEditingController,
-                          style: simpleTextStyle(),
-                          decoration: InputDecoration(
-                              hintText: "Message ...",
-                              hintStyle: TextStyle(
-                                color: Colors.white,
-                                fontSize: 16,
-                              ),
-                              border: InputBorder.none
+                      controller: messageEditingController,
+                      style: simpleTextStyle(),
+                      decoration: InputDecoration(
+                          hintText: "Message ...",
+                          hintStyle: TextStyle(
+                            color: Colors.white,
+                            fontSize: 16,
                           ),
-                        )),
-                    SizedBox(width: 16,),
+                          border: InputBorder.none),
+                    )),
+                    SizedBox(
+                      width: 16,
+                    ),
                     GestureDetector(
                       onTap: () {
                         addMessage();
@@ -115,13 +115,14 @@ class _ChatState extends State<Chat> {
                                     const Color(0x0FFFFFFF)
                                   ],
                                   begin: FractionalOffset.topLeft,
-                                  end: FractionalOffset.bottomRight
-                              ),
-                              borderRadius: BorderRadius.circular(40)
-                          ),
+                                  end: FractionalOffset.bottomRight),
+                              borderRadius: BorderRadius.circular(40)),
                           padding: EdgeInsets.all(12),
-                          child: Image.asset("assets/images/send.png",
-                            height: 25, width: 25,)),
+                          child: Image.asset(
+                            "assets/images/send.png",
+                            height: 25,
+                            width: 25,
+                          )),
                     ),
                   ],
                 ),
@@ -132,7 +133,6 @@ class _ChatState extends State<Chat> {
       ),
     );
   }
-
 }
 
 class MessageTile extends StatelessWidget {
@@ -140,7 +140,6 @@ class MessageTile extends StatelessWidget {
   final bool sendByMe;
 
   MessageTile({@required this.message, @required this.sendByMe});
-
 
   @override
   Widget build(BuildContext context) {
@@ -159,35 +158,25 @@ class MessageTile extends StatelessWidget {
                   fontSize: 16,
                   fontFamily: 'OverpassRegular',
                   fontWeight: FontWeight.w300)),
-          margin: sendByMe
-              ? EdgeInsets.only(left: 30)
-              : EdgeInsets.only(right: 30),
-          padding: EdgeInsets.only(
-              top: 17, bottom: 17, left: 20, right: 20),
+          margin:
+              sendByMe ? EdgeInsets.only(left: 30) : EdgeInsets.only(right: 30),
+          padding: EdgeInsets.only(top: 17, bottom: 17, left: 20, right: 20),
           decoration: BoxDecoration(
-            borderRadius: sendByMe ? BorderRadius.only(
-                topLeft: Radius.circular(23),
-                topRight: Radius.circular(23),
-                bottomLeft: Radius.circular(23)
-            ) :
-            BorderRadius.only(
-                topLeft: Radius.circular(23),
-                topRight: Radius.circular(23),
-                bottomRight: Radius.circular(23)),
+            borderRadius: sendByMe
+                ? BorderRadius.only(
+                    topLeft: Radius.circular(23),
+                    topRight: Radius.circular(23),
+                    bottomLeft: Radius.circular(23))
+                : BorderRadius.only(
+                    topLeft: Radius.circular(23),
+                    topRight: Radius.circular(23),
+                    bottomRight: Radius.circular(23)),
             gradient: LinearGradient(
-              colors: sendByMe ? [
-                const Color(0xff007EF4),
-                const Color(0xff2A75BC)
-              ]
-                  : [
-                const Color(0x1AFFFFFF),
-                const Color(0x1AFFFFFF)
-              ],
-
+              colors: sendByMe
+                  ? [const Color(0xff007EF4), const Color(0xff2A75BC)]
+                  : [const Color(0x1AFFFFFF), const Color(0x1AFFFFFF)],
             ),
-
           ),
         ));
   }
 }
-
